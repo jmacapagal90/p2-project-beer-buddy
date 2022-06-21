b18da4eca10430f1854b305c15b9079c8d967375
James Macapagal <j.macapagal@gmail.com>
2022-06-21 12:34:36 -0500

    updated semantic css stylehsheet link

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+
+document.head.appendChild(styleLink);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
