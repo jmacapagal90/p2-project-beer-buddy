209c61c97c0e7d75b381b68d936bc393933555cd
James Macapagal <j.macapagal@gmail.com>
2022-06-21 09:51:30 -0500

    added route and paths

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,17 @@
 import './App.css';
 import LoginPage from './LoginPage'
-import LandingPage from './LandingPage';
+import Home from './Home';
 import { useState } from 'react';
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  const [isLoggedin, setIsLoggedIn] = useState(false)
-  const helpLogin = () => {
-    setIsLoggedIn(true)
-  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <LoginPage helpLogin={helpLogin}/>
-        {isLoggedin?
-        <LandingPage />:null}
+          <Route path="/">
+            <Home />
+          </Route>
       </header>
     </div>
   );
