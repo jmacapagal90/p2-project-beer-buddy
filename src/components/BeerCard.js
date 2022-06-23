97b4048399ec6de11be513e69677e3b92b0e073d
James Macapagal <j.macapagal@gmail.com>
2022-06-23 14:59:49 -0500

    updated css

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button } from "semantic-ui-react";
+import { Card, Image,Button } from "semantic-ui-react";
 import { FaShoppingCart,FaTrash } from "react-icons/fa";
 
 
@@ -11,21 +11,17 @@ function BeerCard({ beer, addToCart,deleteBeer }){
 
 
     return (
-        <Card>
-        <div className="image">
-          <img src={beer.image} alt={beer.name} />
-        </div>
-        <div className="content">
-          <div className="header">{beer.name}</div>
-        </div>
-        <div className="extra content">
-          <h1>{beer.price}</h1>
-          <Button onClick={handleClick}>{addToCart ? <FaShoppingCart/> : <FaTrash/>}</Button>
-          <span>
-            <i className="icon heartbeat red" />
-            Rating: {Math.round(beer.rating.average * 10)/ 10}
-          </span>
-        </div>
+          <Card>
+            <Image wrapped ui={false} src={beer.image} alt={beer.name} />
+          <Card.Content>
+            <Card.Header>
+              {beer.name}
+            </Card.Header>
+              {beer.price}
+              <Button onClick={handleClick}>{addToCart ? <FaShoppingCart/> : <FaTrash/>}</Button>
+                <i className="icon heartbeat red" />
+                Rating: {Math.round(beer.rating.average * 10)/ 10}
+        </Card.Content>
         </Card>
     )
 }
