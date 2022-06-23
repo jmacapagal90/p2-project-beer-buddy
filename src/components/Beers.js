f32f43b87e7ea97be2702e43386785079d3efdbb
James Macapagal <j.macapagal@gmail.com>
2022-06-23 14:24:43 -0500

    updated beer page styling

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,12 +1,16 @@
 import BeerMenu from './BeerMenu';
-import { Container } from "semantic-ui-react";
+import { Container, Header } from "semantic-ui-react";
 import Search from "./Search";
 
 function Beers({ beers, setSearchQuery, addToCart }){
     //if you havent opened a tab yet or added to existing tab, 
     return (
         <Container>
-            <Search setSearchQuery={setSearchQuery}/>
+            <Header textAlign='center'>Search For Your Favorite Beer!</Header>
+            <Container id='search' textAlign='center'>
+                <Search setSearchQuery={setSearchQuery}/>
+            </Container>
+            <Header textAlign='center'>Browse For Your Favorite Beer!</Header>
             <BeerMenu beers={beers} addToCart={addToCart}/>
         </Container>
     )
