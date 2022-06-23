e15e013393e4c0c50b416cadd109a4e5304abb9c
James Macapagal <j.macapagal@gmail.com>
2022-06-23 10:39:09 -0500

    started work on add new beer

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,4 +1,5 @@
 import BeerMenu from './BeerMenu';
+import BeerForm from './BeerForm';
 import { Container } from "semantic-ui-react";
 import Search from "./Search";
 
@@ -6,6 +7,7 @@ function Beers({ beers, setSearchQuery, addToCart }){
     //if you havent opened a tab yet or added to existing tab, 
     return (
         <Container>
+            <BeerForm />
             <Search setSearchQuery={setSearchQuery}/>
             <BeerMenu beers={beers} addToCart={addToCart}/>
         </Container>
