4b9e14188684d1d35488f8a50114d3f1ddf6ecb6
James Macapagal <j.macapagal@gmail.com>
2022-06-23 11:46:30 -0500

    updating stuff

--- src/components/CloseTab.js
+++ src/components/CloseTab.js
@@ -1,19 +1,30 @@
 import React from "react";
 import BeerCard from "./BeerCard";
-import { CardGroup } from "semantic-ui-react"
+import { Container, List, Button } from "semantic-ui-react"
 
-function CloseTab({activeTab}){
-    const {name,beersOrdered} = activeTab
+function CloseTab({activeTab,onDeleteTab}){
+    const {id, tabName,beersOrdered} = activeTab;
+
+    function handleCloseTab(){
+        fetch(`https://sheltered-beach-53138.herokuapp.com/openTabs/${id}`, {
+            method: 'DELETE',
+    })
+        onDeleteTab(id)
+    }
     return (
-        <CardGroup>
-        <h3>Active Tab: {name}</h3>
-        {beersOrdered.length != 0 ? 
-            beersOrdered.map((beer)=>{
-            return (
-                <BeerCard key={beer.id} beer={beer}/>
-            )
-      }): null}
-        </CardGroup>       
+        <Container className="homeHeader">
+        <h3>Active Tab: {tabName}</h3>
+            {beersOrdered.length != 0 ? 
+                beersOrdered.map((beer)=>{
+                return (
+                    <List>
+                        <List.Item>{beer.name}</List.Item>
+                        <List.Item>{beer.price}</List.Item>
+                    </List>
+                )
+            }): null}
+        <Button type="submit" onSubmit={handleCloseTab}>Close Tab</Button>
+        </Container>       
     )
 }
 
