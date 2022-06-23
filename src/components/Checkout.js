0ee4fd554ac1ddf733c449b69d300701e8501e10
James Macapagal <j.macapagal@gmail.com>
2022-06-23 15:15:20 -0500

    removed old code

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,9 +1,8 @@
 import React, {useState} from "react";
-import { Card, Button, Header, Container, Menu, MenuItem } from "semantic-ui-react";
+import { Card, Button, Container, Menu, MenuItem } from "semantic-ui-react";
 import BeerCard from "./BeerCard";
 
 function Checkout({cart,activeTab,deleteBeer,clearCart, sendActiveTab}){
-  const [closeTab,setCloseTab] = useState(false)
   //if you havent opened a tab yet or added to existing tab, in checkout, if tab is null, create an alert asking to create a tab
 
   function checkoutCart(){
@@ -30,7 +29,7 @@ function Checkout({cart,activeTab,deleteBeer,clearCart, sendActiveTab}){
           <MenuItem> Tab Name: {(activeTab) ? (activeTab.name) : ""}
           </MenuItem>
           <MenuItem>
-            <Button text="Check Out" className={closeTab ? "green" : "grey"} onClick={checkoutCart}>Check Out</Button>
+            <Button text="Check Out" onClick={checkoutCart}>Check Out</Button>
           </MenuItem>
           </Menu>
         <Card.Group itemsPerRow={4}>
