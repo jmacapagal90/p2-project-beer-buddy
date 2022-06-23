5bd6d73976d3141d323c9bdee31308fa815afae5
James Macapagal <j.macapagal@gmail.com>
2022-06-23 15:17:02 -0500

    refactoring

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import { Card, Button, Container, Menu, MenuItem } from "semantic-ui-react";
 import BeerCard from "./BeerCard";
 
